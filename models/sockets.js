43d5d2231224436d49a3b02fe05db5925a1613f2
Francisco de Sautu Riestra <fdesautu@gmail.com>
2022-05-11 20:11:38 -0300

    added gitignore

---
+++ models/sockets.js
@@ -0,0 +1,23 @@
+const BandList = require("./band-list");
+
+class Sockets {
+  constructor(io) {
+    this.io = io;
+
+    this.bandList = new BandList();
+
+    this.socketEvents();
+  }
+
+  socketEvents() {
+    // On connection
+    this.io.on("connection", (socket) => {
+      console.log("Cliente contectado");
+
+      //Emitir al cliente contectado, todas las bandas actuales
+      socket.emit("current-bands", this.bandList.getBands());
+    });
+  }
+}
+
+module.exports = Sockets;
