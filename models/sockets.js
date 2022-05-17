4969cc36d871610296a5e3dfa7d629d39d5acdfe
Francisco de Sautu Riestra <fdesautu@gmail.com>
2022-05-17 18:30:30 -0300

    Update

--- models/sockets.js
+++ models/sockets.js
@@ -16,6 +16,26 @@ class Sockets {
 
       //Emitir al cliente contectado, todas las bandas actuales
       socket.emit("current-bands", this.bandList.getBands());
+
+      socket.on("votar-banda", ({ id }) => {
+        this.bandList.increaseVotes(id);
+        this.io.emit("current-bands", this.bandList.getBands());
+      });
+
+      socket.on("borrar-banda", ({ id }) => {
+        this.bandList.removeBand(id);
+        this.io.emit("current-bands", this.bandList.getBands());
+      });
+
+      socket.on("cambiar-nombre-banda", ({ id, nuevoNombre }) => {
+        this.bandList.changeBandName(id, nuevoNombre);
+        this.io.emit("current-bands", this.bandList.getBands());
+      });
+
+      socket.on("agregar-banda", ({ nombreNuevaBanda }) => {
+        this.bandList.addBand(nombreNuevaBanda);
+        this.io.emit("current-bands", this.bandList.getBands());
+      });
     });
   }
 }
